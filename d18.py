d9e5a11bbf12143b16a9f46924b5f0772fb79ff4
James Fowkes <jamesfowkes@gmail.com>
2015-12-31 09:46:18 +0000

    Completed day 18

--- d18.py
+++ d18.py
@@ -1,23 +1,35 @@
 import sys
 import copy
+from collections import namedtuple
+
+Grid = namedtuple('Grid', ['grid', 'rows', 'cols'])
 
 def get_blank_grid(rows, cols):
-	return [[0 for _x in range(cols)] for _y in range(rows)] 
+	grid = [[0 for _x in range(cols)] for _y in range(rows)] 
+	return Grid(grid, rows, cols)
 
-def get_initial_grid(rows, cols):
+def get_initial_grid():
+
+	lines = [l.strip() for l in sys.stdin]
+
+	rows = len(lines)
+	cols = len(lines[0])
 
 	grid = get_blank_grid(rows, cols)
-	for r, line in enumerate(sys.stdin):
-		line = line.strip()
+
+	for r, line in enumerate(lines):
 		for c, on_off in enumerate(line):
-			grid[r][c] = on_off == '#'
+			grid.grid[r][c] = on_off == '#'
 
 	return grid
 
-def count_if_on(row, col, grid):
+def count_if_on(row, col, grid, _print=False):
+
+	if row < 0 or col < 0:
+		return 0
 
 	try:
-		return 1 if grid[row-1][col-1] else 0
+		return 1 if grid[row][col] else 0
 	except:
 		return 0
 
@@ -25,7 +37,7 @@ def count_neighbours(row, col, grid):
 
 	n = 0
 
-	n += count_if_on(row-1, col-1, grid)
+	n += count_if_on(row-1, col-1, grid, row==0 and col == 0)
 	n += count_if_on(row-1, col, grid)
 	n += count_if_on(row-1, col+1, grid)
 
@@ -38,20 +50,36 @@ def count_neighbours(row, col, grid):
 
 	return n
 
-def run_grid(grid, rows, cols):
-	new_grid = copy.deepcopy(grid)
+def run_grid(grid):
+	new_grid = copy.deepcopy(grid.grid)
 
-	for row in range(rows):
-		for col in range(cols):
-			neighbours = count_neighbours(row, col, grid)
+	for row in range(grid.rows):
+		for col in range(grid.cols):
+			neighbours = count_neighbours(row, col, grid.grid)
 
-			if grid[row][col] and (neighbours < 2 or neighbours > 3):
+			if grid.grid[row][col] and (neighbours < 2 or neighbours > 3):
 				new_grid[row][col] = False
 
-			if not grid[row][col] and (neighbours == 3):
+			if not grid.grid[row][col] and (neighbours == 3):
 				new_grid[row][col] = True
 
-	return new_grid
+	return Grid(new_grid, grid.rows, grid.cols)
+
+def set_on(grid, r, c):
+	new_grid = copy.deepcopy(grid.grid)
+	
+	new_grid[r][c] = True
+
+	return Grid(new_grid, grid.rows, grid.cols)
+	
+def count_on(grid):
+
+	count = 0
+	for row in grid.grid:
+		for col in row:
+			count += 1 if col else 0
+
+	return count
 
 def print_row(row):
 	row = ['#' if c else '.' for c in row]
@@ -59,16 +87,21 @@ def print_row(row):
 
 def print_grid(grid):
 
-	for row in grid:
+	for row in grid.grid:
 		print_row(row)
 	
 if __name__ == "__main__":
 
-	grid = get_initial_grid(6, 6)
-
 	steps = int(sys.argv[1])
 
+	grid = get_initial_grid()
+
 	for s in range(steps):
-		grid = run_grid(grid, 6, 6)
+		grid = run_grid(grid)
+		grid = set_on(grid, 0, 0)
+		grid = set_on(grid, grid.rows-1, 0)
+		grid = set_on(grid, 0, grid.cols-1)
+		grid = set_on(grid, grid.rows-1, grid.cols-1)
 
 	print_grid(grid)
+	print(count_on(grid))
